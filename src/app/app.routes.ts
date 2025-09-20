bf6b8b0c53218f6b303a9cbbb15314b334f3cd1d
Lucas Marques <lucaamarques2406@gmail.com>
2025-09-19 22:19:25 -0300

    configurando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Auth } from './components/auth/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //rota para paginda de login 
+    {path: "", redirectTo: "auth", pathMatch: "full",},
+    {path: "auth", component: Auth,},
+    {path: "**", component: Auth},
+
+];
